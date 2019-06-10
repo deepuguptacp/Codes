6b7c878941a73025954e0e2846f6c992d8574c08
Deepu Gupta <deepugupta@Deepus-MacBook-Pro.local>
2019-06-10 19:44:13 +0530

    new

---
+++ c_codes/new.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+    int num;
+    printf("Input any number : "); 
+    scanf("%d", &num);             
+    printf("Number = %d", num);    
+    printf("\n");                  
+    return 0;
+}
