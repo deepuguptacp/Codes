578169b329dc9fba6bfec638f90a1db41460baad
Deepu Gupta <deepugupta@Deepus-MacBook-Pro.local>
2019-06-17 20:47:32 +0530

    Change

--- c++_codes/Data_Structure/hashing.cpp
+++ c++_codes/Data_Structure/hashing.cpp
@@ -2,49 +2,42 @@
 #include<iostream>
 #include<vector>
 using namespace std;
-int hashing(int n)
-{
+int hashing(int n){
 	int temp=n;
 	int sum=0;
-	while(temp!=0)
-	{
-	int k=temp%10;
-	sum+=k;
-	temp/=10;
+	while(temp!=0){
+		int k=temp%10;
+		sum+=k;
+		temp/=10;
 	}
 	return sum;
 }
-string search(vector<int> v[],int n)
-{
+string search(vector<int> v[],int n){
 	int temp=hashing(n);
 	vector<int> ::iterator it=v[temp].begin();
-	while(it!=v[temp].end())
-	{
+	while(it!=v[temp].end()){
 		if(*it==n)
 	     return "Element found ";
 	     it++;
 	}
 	return "Not Found ";
 }
-int main()
-{
- int n;
- cout<<"Enter the number of elements u want to insert ";
- cin>>n;
- vector<int> vec[100];
- cout<<"Enter the values in the array ";
- for(int i=0;i<n;i++)
- {
- 	int val;
- 	cin>>val;
- 	int temp=hashing(val);
- 	vec[temp].push_back(val);
-
- }
- cout<<"Enter the element to be searched ";
- int e;
- cin>>e;
- cout<<search(vec,e);
- cout<<"\n";
- return 0;
+int main(){
+ 	int n;
+ 	cout<<"Enter the number of elements u want to insert ";
+ 	cin>>n;
+ 	vector<int> vec[100];
+ 	cout<<"Enter the values in the array ";
+ 	for(int i=0;i<n;i++){
+ 		int val;
+ 		cin>>val;
+ 		int temp=hashing(val);
+ 		vec[temp].push_back(val);
+ 	}
+ 	cout<<"Enter the element to be searched ";
+ 	int e;
+ 	cin>>e;
+ 	cout<<search(vec,e);
+ 	cout<<"\n";
+ 	return 0;
 }
