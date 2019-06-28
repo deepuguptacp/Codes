e40d551a1891e3b18b7b2468ae806059941f44ea
Deepu Gupta <deepugupta@Deepus-MacBook-Pro.local>
2019-06-28 11:18:51 +0530

    FCTRL

--- c++_codes/Codechef/FCTRL2.cpp
+++ c++_codes/Codechef/FCTRL2.cpp
@@ -3,7 +3,39 @@
 
 using namespace std;
 
-void summation(string, string);
+string summation(string a, string b){
+    string str = "";
+    int i, carry=0, x, y, sum, data;
+    i=a.length()-1;
+    int j=b.length()-1;
+    while(i>=0 && j>=0){
+        x=a[i--]-'0';
+        y=b[j--]-'0';
+        sum=x+y+carry;
+        data=sum%10;
+        carry=sum/10;
+        str.push_back(data+'0');
+    }
+    while(i>=0 && j<0){
+        x=a[i--]-'0';
+        sum=x+carry;
+        data=sum%10;
+        carry=sum/10;
+        str.push_back(data+'0');
+    }
+    while(j>=0 && i<0){
+        y=b[j--]-'0';
+        sum=y+carry;
+        data=sum%10;
+        carry=sum/10;
+        str.push_back(data+'0');
+    }
+    if(carry!=0){
+        str.push_back(carry+'0');
+    }
+    reverse(str.begin(), str.end()); 
+    return str;
+}
 
 string product(string a, int n){
     string str = "";
