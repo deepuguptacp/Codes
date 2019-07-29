28e37f0a6a1ede4a66dc2dea34496c8813ca4fd8
Deepu Gupta <199deepu@gmail.com>
2019-07-29 22:42:21 +0530

    Codechef

--- Codechef/pair_problem.cpp
+++ Codechef/pair_problem.cpp
@@ -5,14 +5,7 @@ int numberOfWays(int x)
 { 
     // Base condition  
     if (x==0 || x==1)      
-        return 1; 
-  
-    // A participant can choose to 
-    // (1) Remains single. Number of people  
-    //     reduce to (x-1) 
-    // (2) Pairs with one of the (x-1) others. 
-    //     For every pairing, number of people  
-    //     reduce to (x-2).  
+        return 1;
     else 
         return numberOfWays(x-1) + (x-1)*numberOfWays(x-2); 
 } 
