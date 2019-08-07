09f8a45c8973041df0e55b9008ae7989f3b34a09
Deepu Gupta <199deepu@gmail.com>
2019-08-07 22:39:58 +0530

    Compilers

---
+++ compiler/checkFollowAndFirst.c
@@ -0,0 +1,11 @@
+#include<stdio.h> 
+#include<ctype.h> 
+#include<string.h> 
+char production[10][100];
+int main(){
+    char str[100]="";
+    scanf("%s", str);
+    strcpy(production, "str");
+    printf("%s", production);
+    return 0;
+}
