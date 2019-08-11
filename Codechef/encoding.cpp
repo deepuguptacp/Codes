15f1147077e8ea8e188b5a1fd0adbefd1e3a3114
Deepu Gupta <199deepu@gmail.com>
2019-08-11 20:45:50 +0530

    encoding

---
+++ Codechef/encoding.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+    long long n;
+    cin>>n;
+    while(n--){
+        long int Nr, Nl, L, R, i, rslt=0;
+        cin>>L;
+        cin>>Nl;
+        cin>>R;
+        cin>>Nr;
+        for(i=L; i<R; i++){
+            
+        }
+    }
+    return 0;
+}
