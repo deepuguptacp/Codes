fac112d23245d24ffdc53682b5933ea504d444af
Deepu Gupta <199deepu@gmail.com>
2019-08-20 23:26:44 +0530

    Codexhef

---
+++ Codechef/Guddu_and_his_mother.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+// your code goes here
+    long test,count,c,i,x,ans,sum;
+    vector<long> a;
+    vector<long> xora;
+    map<long,vector<long>> mp ;
+
+    cin>>test;
+    while(test--) {
+        ans=0;
+        mp.clear();
+        a.clear();
+        xora.clear();
+        cin>>count;
+        cin>>x;
+        a.push_back(x);
+        xora.push_back(x);
+        mp.insert (pair<long,vector<long>>(x,{0}));
+        for(i=1;i<count;i++) {
+            cin>>x;
+            a.push_back(x);
+            xora.push_back(xora[i-1]^x);
+            mp[xora[i]].push_back(i);
+        }
+        for (auto& x: mp) {
+            long l=x.second.size();
+            if(l>1) {
+                sum=0;
+                for(long j=0;j<l;j++) {
+                    sum += (x.second[j]*(j) -x.second[j]*(l-j-1));
+                }
+                ans+=sum - (l*(l-1)/2);
+            }
+            if(x.first==0) {
+                for(long j=0;j<l;j++) {
+                    ans=ans+x.second[j];
+                }
+            }
+        }
+        cout<<ans<<endl;
+    }
+    return 0;
+}
--- Codechef/SmartPhone.cpp
+++ Codechef/SmartPhone.cpp
@@ -1,13 +1,21 @@
 #include<iostream>
+#include<vector>
+#include<algorithm>
+
 using namespace std;
 int main(){
-    long long n, max=0;
+    long long n, i, a;
     cin>>n;
-    long long arr[n], i;
+    vector<int> arr, b;
     for(i=0; i<n; i++){
-        cin>>arr[i];
+        cin>>a;
+        arr.push_back(a);
     }
+    sort(arr.begin(), arr.end());
     for(i=0; i<n; i++){
-        
+        b.push_back(arr[i]*(n-i));
     }
+    sort(b.begin(), b.end());
+    cout<<b[n-1]<<endl;
+    return 0;
 }
