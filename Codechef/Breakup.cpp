6b329efedad2bc9fccae6c9c19ff7502a92645ff
Deepu Gupta <199deepu@gmail.com>
2019-08-21 16:14:13 +0530

    BreakUp

--- Codechef/Breakup.cpp
+++ Codechef/Breakup.cpp
@@ -5,9 +5,12 @@ using namespace std;
 #define ll long long
 
 int main(){
-    ll n, count=0;
+    ll n, count=0, i=0;
     cin>>n;
     vector<int> V;
-
+    for(i=0; i<n; i++){
+        cin>>V[i];
+    }
+    
     return 0;
 }
