7e09d185447bf85d6ee161c8587c2a6ef2ba7e6e
Deepu Gupta <199deepu@gmail.com>
2019-08-26 14:30:18 +0530

    FCFS

--- Class/Operating system/FCFS.cpp
+++ Class/Operating system/FCFS.cpp
@@ -1,26 +1,17 @@
 #include<iostream>
-
 using namespace std;
 int i=0;
 
-int waiting_time(int V[], int n){
-    int wt=0;
-    for(i=0; i<n-1; i++){
-        wt=wt+V[i];
-    }
-    return wt;
-}
-
 int main(){
     int n;
-    cout<<"Enter the number of Processes : ";
+    cout<<"Enter the number of processes : ";
     cin>>n;
-    int burst_time[n], process_no[n];
+    int burtTime[n], processNo[n];
     for(i=0; i<n; i++){
-        process_no[i]=i+1;
-        cout<<"Enter the burst time for "<<i+1<<"th process : ";
-        cin>>burst_time[i];
+        cout<<"Burst Time for Process "<<i+1<<" : "<<endl;
+        cin>>burtTime[i];
+        processNo[i]=i+1;
     }
-    int wt = waiting_time(burst_time, n);
-    cout<<"Waiting time : "<<wt<<endl;
+    
+    return 0;
 }
