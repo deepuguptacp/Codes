72a8d85440a31b022e0c8c2b657ade24ba175e6b
Deepu Gupta <199deepu@gmail.com>
2019-08-26 22:33:38 +0530

    ZOMCAV

--- Codechef/ZombieAndTheCaves.cpp
+++ Codechef/ZombieAndTheCaves.cpp
@@ -1,15 +1,17 @@
 #include<iostream>
 #include<algorithm>
+#include<math.h>
 
 using namespace std;
+
 int main(){
     long int n;
     cin>>n;
     while(n--){
-        long int m, temp=0, i=0, j=0;
+        long long int m, temp=0, i=0, j=0, zero=0;
         cin>>m;
-        long long C[m], H[m]; 
-        long long radiationLevel[m];
+        long long int C[m], H[m]; 
+        long long int radiationLevel[m];
         for(i=0; i<m; i++){
             cin>>C[i];
         }
@@ -17,9 +19,16 @@ int main(){
             radiationLevel[i]=0;
         }
         for(i=0; i<m; i++){
-            for(j=i+1-C[i]; j<i+1+C[i]; j++){
-                radiationLevel[j]++;
-            }
+            long long int x = i-C[i];
+            long long int y = i+C[i];
+            x = max(zero, x);
+            y = min(m-1, y);
+            radiationLevel[x]+=1;
+            if(y!=m-1)
+                radiationLevel[y+1]-=1;
+        }
+        for(i=1; i<m; i++){
+            radiationLevel[i]=radiationLevel[i-1]+radiationLevel[i];
         }
         for(i=0; i<m; i++){
             cin>>H[i];
