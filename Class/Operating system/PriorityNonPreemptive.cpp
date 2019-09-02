45e755be3133d5cacce592451dbc082369a54999
Deepu Gupta <199deepu@gmail.com>
2019-09-02 15:16:47 +0530

    OS

--- Class/Operating system/PriorityNonPreemptive.cpp
+++ Class/Operating system/PriorityNonPreemptive.cpp
@@ -2,6 +2,7 @@
 #include<algorithm>
 #include<vector>
 #include<utility>
+#include<string>
 using namespace std;
 int main(){
     int n, i;
--- Class/Operating system/SJFPreemptive.cpp
+++ Class/Operating system/SJFPreemptive.cpp
@@ -8,6 +8,12 @@ int main(){
     cout<<"Enter the number of processes : ";
     cin>>n;
     vector<pair<int, int> > P(n);
+    int ProcessNo[n], WaitingTime[n], turnAroundTime[n];
+    for(i=0; i<n; i++){
+        cout<<"Enter the Arrival Time and Burst Time for process "<<i+1<<" respectively : ";
+        cin>>P[i].first>>P[i].second;
+        ProcessNo[i]=i+1;
+    }
     
 
     return 0;
