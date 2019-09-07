9ff2cefb43f9a5acc1ad7441b72fca2307e4d99b
Deepu Gupta <199deepu@gmail.com>
2019-09-08 01:20:38 +0530

    codechef

--- Codechef/InterestingSum.cpp
+++ Codechef/InterestingSum.cpp
@@ -7,12 +7,14 @@ int main(){
     int t;
     cin>>t;
     while(t--){
-        int N, K, i=0, temp=0, j=0, sum=0, min=0;
+        int N, K, i=0, temp=0, j=0, sum=0, minsum=0;
         cin>>N>>K;
         int A[N];
         for(i=0; i<N; i++)
             cin>>A[i];
         sort(A, A + N);
+        for(i=0; i<K; i++)
+            minsum += A[i];
         
     }
     return 0;
