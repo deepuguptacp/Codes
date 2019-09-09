22504a872fc7ebd63ee7a19a13b0a5483acc891d
Deepu Gupta <199deepu@gmail.com>
2019-09-09 13:53:18 +0530

    Codechef

--- Codechef/InterestingSum.cpp
+++ Codechef/InterestingSum.cpp
@@ -1,13 +1,31 @@
 //https://www.codechef.com/SEPT19B/problems/CHEFINSQ
-#include<iostream>
+#include <iostream>
 #include<algorithm>
-using namespace std;
-
-int main(){
-    int t;
+using namespace std; 
+int minsum=0, temp=0;
+void combinationUtil(int arr[], int n, int r, int index, int data[], int i);
+void printCombination(int arr[], int n, int r) {
+	int data[r]; 
+	combinationUtil(arr, n, r, 0, data, 0); 
+}
+void combinationUtil(int arr[], int n, int r, int index, int data[], int i){
+	if (index == r){
+        int count=0;
+		for (int j = 0; j < r; j++){
+			count=count+data[j];
+        }
+        if(count==minsum)
+            temp++; 
+	}
+	//data[index] = arr[i]; 
+	combinationUtil(arr, n, r, index + 1, data, i + 1);
+	combinationUtil(arr, n, r, index, data, i + 1); 
+}
+int main(){ 
+	int t;
     cin>>t;
     while(t--){
-        int N, K, i=0, minsum=0, count=1;
+        int N, K, i=0, count=0;
         cin>>N>>K;
         int A[N];
         for(i=0; i<N; i++)
@@ -15,13 +33,8 @@ int main(){
         sort(A, A + N);
         for(i=0; i<K; i++)
             minsum += A[i];
-        int curr_sum = minsum; 
-        for (int i=K; i<N; i++){ 
-            curr_sum += A[i] - A[i-K];
-            if(curr_sum == minsum)
-                count++;
-        }
-        cout<<count<<endl;
+        printCombination(A, N, K);
+        cout<<temp<<endl;
     }
     return 0;
-}
+} 
