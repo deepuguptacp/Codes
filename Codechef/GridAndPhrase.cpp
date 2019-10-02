9f2c0c55885a520f01ca925d78395fbca6a8dcf5
Deepu Gupta <199deepu@gmail.com>
2019-10-02 22:34:55 +0530

    Codechef

--- Codechef/GridAndPhrase.cpp
+++ Codechef/GridAndPhrase.cpp
@@ -2,11 +2,38 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int n, m;
+    int n, m, temp=0;
+    int i=0, j=0;
     cin>>n>>m;
-    char A[m];
-    for(int i=0; i<n; i++)
-        cin>>A;
-    
+    char A[n][m];
+    for(i=0; i<n; i++)
+        for(j=0; j<m; j++)
+            cin>>A[i][j];
+    for(i=0; i+3<n; i++){
+        for(j=0; j<m; j++){
+            if(A[i][j]=='s' && A[i+1][j]=='a' && A[i+2][j]=='b' && A[i+3][j]=='a')
+                temp++;
+        }
+    }
+    for(i=0; i<n; i++){
+        for(j=0; j+3<m; j++){
+            if(A[i][j]=='s' && A[i][j+1]=='a' && A[i][j+2]=='b' && A[i][j+3]=='a')
+                temp++;
+        }
+    }
+    for(i=0; i+3<n; i++){
+        for(j=0; j+3<m; j++){
+            if(A[i][j]=='s' && A[i+1][j+1]=='a' && A[i+2][j+2]=='b' && A[i+3][j+3]=='a')
+                temp++;
+        }
+    }
+    for(i=0; i-3<n; i++){
+        for(j=0; j+3<m; j++){
+            if(A[i][j]=='s' && A[i-1][j+1]=='a' && A[i-2][j+2]=='b' && A[i-3][j+3]=='a')
+                temp++;
+        }
+    }
+
+    cout<<temp<<endl;
     return 0;
 }
