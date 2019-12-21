aea623a1ccb0da8559605cd04dd9d814ff97aecf
deepuguptacp <199deepu@gmail.com>
2019-12-21 08:54:14 +0530

    Dijkastra Algorithm

--- Class/NetworkingLab/DijkshtraAlgorithm.cpp
+++ Class/NetworkingLab/DijkshtraAlgorithm.cpp
@@ -3,7 +3,7 @@
 #include<vector>
 using namespace std;
 //int distance[8][8];
-#define max 2
+#define max 8
 #define INFINITY 9999
 int priority(char ch){
     if( ch == 'A'|| ch == 'a' )
@@ -23,15 +23,33 @@ int priority(char ch){
     else if( ch == 'H' || ch == 'h')
         return 7;
 }
-//void dijkstra(int G[max][max],int n,int startnode);
-/*void dijkastra(G[max][max], int src, int dest){
-    int dist;
-    
-}*/
+
+int minDistance(int dist[], bool sptSet[]){
+	int min = INT_MAX, min_index;
+	for (int v = 0; v < max; v++)
+		if (sptSet[v] == false && dist[v] <= min)
+			min = dist[v], min_index = v;
+	return min_index; 
+} 
+
+void dijkastra(int G[max][max], int src, int dest){
+    int dist[max];
+	bool sptSet[max]; // sptSet[i] will be true if vertex i is included in shortest 
+	for (int i = 0; i < max; i++) 
+		dist[i] = INT_MAX, sptSet[i] = false;
+	dist[src] = 0;  // Distance of source vertex from itself is always 0
+	for (int count = 0; count < max - 1; count++){
+		int u = minDistance(dist, sptSet);
+		sptSet[u] = true;
+		for (int v = 0; v < max; v++)
+			if (!sptSet[v] && G[u][v] && dist[u] != INT_MAX && dist[u] + G[u][v] < dist[v]) 
+				dist[v] = dist[u] + G[u][v]; 
+	} 
+    cout<<endl;
+    cout<<"The minimum distance between Source and Destination is : "<<dist[dest]<<" units"<<endl;
+}
+
 int main(){
-    /*int n;
-    cout<<"Enter the number of nodes : ";
-    cin>>n;*/
     int distance[max][max];
     cout<<"Enter the distance between the nodes serially : "<<endl;
     for(int i=0; i<max; i++){
@@ -41,55 +59,8 @@ int main(){
     char start, end;
     cout<<"Enter the starting and End nodes : ";
     cin>>start>>end;
-    cout<<start<<"\t"<<end<<endl; 
     int begin = priority(start);
     int last = priority(end);
-    cout<<begin<<"\t"<<last<<endl;
-    //dijkstra(distance, begin, last);
+    dijkastra(distance, begin, last);
     return 0;
 }
-
-/*void dijkstra(int G[max][max], int n, int startnode){
-    int cost[max][max],distance[max],pred[max];
-   int visited[max],count,mindistance,nextnode,i,j;
-   for(i=0;i<n;i++)
-      for(j=0;j<n;j++)
-   if(G[i][j]==0)
-      cost[i][j]=INFINITY;
-   else
-      cost[i][j]=G[i][j];
-   for(i=0;i<n;i++) {
-      distance[i]=cost[startnode][i];
-      pred[i]=startnode;
-      visited[i]=0;
-   }
-   distance[startnode]=0;
-   visited[startnode]=1;
-   count=1;
-   while(count<n-1) {
-      mindistance=INFINITY;
-      for(i=0;i<n;i++)
-         if(distance[i]<mindistance&&!visited[i]) {
-         mindistance=distance[i];
-         nextnode=i;
-      }
-      visited[nextnode]=1;
-      for(i=0;i<n;i++)
-         if(!visited[i])
-      if(mindistance+cost[nextnode][i]<distance[i]) {
-         distance[i]=mindistance+cost[nextnode][i];
-         pred[i]=nextnode;
-      }
-      count++;
-   }
-   for(i=0;i<n;i++)
-   if(i!=startnode) {
-      cout<<"\nDistance of node"<<i<<"="<<distance[i];
-      cout<<"\nPath="<<i;
-      j=i;
-      do {
-         j=pred[j];
-         cout<<"<-"<<j;
-      }while(j!=startnode);
-   }
-}*/
