07c19fd7ac98e12c6d028cd96ada1c926e8a68d4
Deepu Gupta <deepugupta@Deepus-MacBook-Pro.local>
2020-02-10 18:43:41 +0530

    CC

--- CompetativeCoding/TheTheatreProblem.cpp
+++ CompetativeCoding/TheTheatreProblem.cpp
@@ -2,23 +2,37 @@
 #include<bits/stdc++.h>
 using namespace std;
 #define ll long long
-
+// 12 - P, 3 - Q, 6 - R, 9 - S
 int main(){
     ll T;
     cin>>T;
     while(T--){
         ll N;
         cin>>N;
-        map<char, int> m[N];
+        map<char, map<int, int> > V;
         for(int i=0; i<N; i++){
-            cin>>m[i].first();
-            cin>>m[i].second();
+            int it;
+            char c;
+            cin>>c>>it;
+            V[c][it]++;
         }
-        for(int i=0; i<N; i++){
-            cout<<m[i].first()<<" ";
-            cout<<m[i].second()<<endl;
+        /*for (auto &&i : V)
+        {
+            cout<<i.first<<" ";
+            for (auto &&j : i.second)
+            {
+                cout<<j.first<<"= "<<j.second<<" ";
+            }
+            cout<<endl;
+        }*/
+        for(auto itr = V.begin(); itr!=V.end(); itr++){
+            for (auto itr2 =itr->second.begin(); itr2!=itr->second.end(); itr2++){
+                cout<<itr->first<<" "<<itr2->first<<"="<<itr2->second<<" ";
+            }
+            cout<<endl;
         }
-
+        
+        
 
     }
     return 0;
