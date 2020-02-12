b40d6ad095d0feda2dfeb2a97e1c9a9bead14952
Deepu Gupta <deepugupta@Deepus-MacBook-Pro.local>
2020-02-12 13:04:00 +0530

    GG

---
+++ CompetativeCoding/GeeksforGeeks/StockBuyAndSell.cpp
@@ -0,0 +1,18 @@
+//https://practice.geeksforgeeks.org/problems/stock-buy-and-sell/0/
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+int main(){
+	int T;
+    cin>>T;
+    while(T--){
+        int N;
+        cin>>N;
+        int A[N];
+        for(int i=0; i<N; i++)
+            cin>>A[i];
+        
+    }
+	return 0;
+}
