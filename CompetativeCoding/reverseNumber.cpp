23bc36a3f483e21a624f78ce4ad00aa866300c6a
deepuguptacp <199deepu@gmail.com>
2020-03-16 09:01:29 +0530

    c

---
+++ CompetativeCoding/reverseNumber.cpp
@@ -0,0 +1,12 @@
+/* Suppose If INT_MAX=200 then reverse a number.
+    ex :-   if n = 124
+            reverse = 421(is a overflow)
+            else if n = 91
+            reverse = 19 (Valid number)
+*/
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    
+    return 0;
+}
