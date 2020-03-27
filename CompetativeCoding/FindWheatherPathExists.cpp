bebcb4d432bab5a8ed51410ee3dd64de5db9d915
deepuguptacp <199deepu@gmail.com>
2020-03-27 15:35:59 +0530

    GFG

---
+++ CompetativeCoding/FindWheatherPathExists.cpp
@@ -0,0 +1,19 @@
+//https://practice.geeksforgeeks.org/problems/find-whether-path-exist/0
+// BFS Graph Matrix
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+	int T;
+    cin>>T;
+    while(T--){
+        int n;
+        cin>>n;
+        int A[n][n];
+        for(int i=0; i<n; i++)
+            for(j=0; j<n; j++)
+                cin>>A[i][j];
+        
+    }
+	return 0;
+}
