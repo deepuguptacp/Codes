70083290ee4aa043750ed6a5595957a42f7b4621
deepuguptacp <199deepu@gmail.com>
2020-03-27 15:19:15 +0530

    GFG

---
+++ CompetativeCoding/LongLongDecimal.cpp
@@ -0,0 +1,27 @@
+//https://practice.geeksforgeeks.org/problems/long-long-decimal/0/
+// 355/113 upto k decimal places
+#include<bits/stdc++.h>
+using namespace std;
+int x = 355;
+int y = 113;
+int main(){
+    int T;
+    cin>>T;
+    while(T--){
+        int k;
+        cin>>k;
+        int d = x/y;
+        for (int i = 0; i <= k; i++) { 
+            cout << d; 
+            x = x - (y * d); 
+            if (x == 0) 
+                break; 
+            x = x * 10; 
+            d = x / y; 
+            if (i == 0) 
+                cout << "."; 
+        }
+        cout<<endl;
+    }
+    return 0;
+}
