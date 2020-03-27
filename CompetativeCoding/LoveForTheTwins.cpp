63f2c3005b4bdd31030b57a676cd618764731471
deepuguptacp <199deepu@gmail.com>
2020-03-27 14:36:13 +0530

    GFG

--- CompetativeCoding/LoveForTheTwins.cpp
+++ CompetativeCoding/LoveForTheTwins.cpp
@@ -5,12 +5,26 @@ int main(){
     int T;
     cin>>T;
     while(T--){
-        int n;
+        int n, ans = 0;
         cin>>n;
         int A[n];
-        for(int i=0; i<n; i++)
-            cin>>A[i];
-        
+        map<int, int> m;
+        for(int i=0; i<n; i++){
+            int num;
+            cin>>num;
+            if(m.find(num) == m.end()){
+	            m[num] = 1;
+	        }
+	        else{
+	            m[num]++;
+	        }
+        }
+            map<int, int> :: iterator it = m.begin();
+	        while(it != m.end()){
+	            ans = ans + ((it->second)/2)*2;
+	            it++;
+	        }
+	        cout<<ans<<endl;
     }
     return 0;
 }
