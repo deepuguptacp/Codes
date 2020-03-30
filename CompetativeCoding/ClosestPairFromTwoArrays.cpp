f8500db330a0e951ab95187ee5954481f84414b4
deepuguptacp <199deepu@gmail.com>
2020-03-30 20:09:56 +0530

    GFG

---
+++ CompetativeCoding/ClosestPairFromTwoArrays.cpp
@@ -0,0 +1,35 @@
+//https://practice.geeksforgeeks.org/problems/find-the-closest-pair-from-two-arrays/0/
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+int main(){
+	int t;
+    cin>>t;
+    while(t--){
+        int n, m;
+        cin>>n>>m;
+        int A[n], B[m];
+        for(int i=0; i<n; i++)
+            cin>>A[i];
+        for(int i=0; i<n; i++)
+            cin>>B[i];
+        int x;
+        cin>>x;
+        sort(a,a+n);
+        sort(b,b+m);
+        int g = 0, h = 0;
+        int min = INT_MAX;
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(abs(a[i]+b[j]-x) < min){
+                    min = abs(a[i] + b[j] - x);
+                    g = a[i];
+                    h = b[j];
+                }
+            }
+        }
+        cout<<g<<" "<<h<<endl;
+    }
+	return 0;
+}
