f77d366145923144dc8d11231f8c855d8303bab1
deepuguptacp <199deepu@gmail.com>
2020-03-31 20:34:21 +0530

    gfg

---
+++ CompetativeCoding/MergeWithoutExtraSpace.cpp
@@ -0,0 +1,37 @@
+//https://practice.geeksforgeeks.org/problems/merge-two-sorted-arrays/0
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+void MergeSort(int p[], int q[], int x, int y){
+    for (int i = y-1; i >= 0; i--){
+        int j, last = p[x-1];
+        for (j = x-2; j >= 0 && p[j] > q[i]; j--)
+            p[j+1] = p[j];
+        if (j != x-2 || last > q[i]){ 
+            p[j+1] = q[i];
+            q[i] = last;
+        }
+    }
+}
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int x, y;
+        cin>>x>>y;
+        int p[x], q[y];
+        for(int i=0; i<x; i++)
+            cin>>p[i];
+        for(int i=0; i<y; i++)
+            cin>>q[i];
+        MergeSort(p, q, x, y);
+        for(int i=0; i<x; i++)
+            cout<<p[i]<<" ";
+        for(int i=0; i<y; i++)
+            cout<<q[i]<<" ";
+        cout<<endl;
+    }
+	return 0;
+}
