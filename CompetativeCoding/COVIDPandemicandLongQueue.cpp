487d1006578898cfbc3ffa1cf51adde4b82aca6e
deepuguptacp <199deepu@gmail.com>
2020-04-05 17:14:57 +0530

    gfg

--- CompetativeCoding/COVIDPandemicandLongQueue.cpp
+++ CompetativeCoding/COVIDPandemicandLongQueue.cpp
@@ -11,22 +11,28 @@ int main(){
         int a[n];
         for(int i=0; i<n; i++)
             cin>>a[i];
+        int count = 1;
+        int i=0;
         int min_val = INT_MAX;
-        int temp = 1;
+        while(a[i]==0){
+            i++;
+        }
         for(int i=0; i<n; i++){
-            if(a[i]==0){
-                temp++;
-            }
-            else{
-                min_val = min(min_val, temp);
-                temp = 0;
+            if(a[i]==1){
+                int j=i+1;
+                while(a[j] == 0){
+                    count++;
+                    j++;
+                }
+                min_val = min(min_val, count);
+                i=j+1;
             }
         }
         if(min_val >= 6)
             cout<<"YES"<<endl;
         else
             cout<<"NO"<<endl;
-        
+        //cout<<min_val<<endl;
     }
     return 0;
 }
