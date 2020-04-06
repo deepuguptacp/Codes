ca54d165e76e5309e84b4157d5dc000430bcfa96
deepuguptacp <199deepu@gmail.com>
2020-04-06 07:45:10 +0530

    gfg

--- CompetativeCoding/StrangeNumber.cpp
+++ CompetativeCoding/StrangeNumber.cpp
@@ -1,1 +1,14 @@
 //https://www.codechef.com/APRIL20B/problems/STRNO
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+int main(){
+    ll t;
+    cin>>t;
+    while(t--){
+        ll x, k;
+        cin>>x>>k;
+        
+    }
+    return 0;
+}
