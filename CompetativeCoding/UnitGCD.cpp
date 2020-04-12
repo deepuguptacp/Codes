51d801dddedb698ee9db9f2636a84b246a4a8027
deepuguptacp <199deepu@gmail.com>
2020-04-12 07:17:52 +0530

    Codechef

--- CompetativeCoding/UnitGCD.cpp
+++ CompetativeCoding/UnitGCD.cpp
@@ -4,15 +4,6 @@ using namespace std;
 
 #define ll long long
 
-bool isPrime(int n){
-    if (n <= 1)
-        return false;
-    for (int i = 2; i < n; i++)
-        if (n % i == 0)
-            return false;
-    return true;
-}
-
 int main(){
     ll t;
     cin>>t;
