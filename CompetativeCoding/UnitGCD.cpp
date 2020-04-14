74a526321cc635ec462a21a2a14b77b5dd3d845a
deepuguptacp <199deepu@gmail.com>
2020-04-14 19:32:37 +0530

    Codechef

--- CompetativeCoding/UnitGCD.cpp
+++ CompetativeCoding/UnitGCD.cpp
@@ -1,19 +1,33 @@
 //https://www.codechef.com/APRIL20B/problems/UNITGCD
-#include <iostream>
-#include <vector>
-#include <numeric>
+#include<bits/stdc++.h>
 using namespace std;
 
 #define ll long long
 
 int main(){
     ll t;
-    cin>>t;
-    while(t--){
-        ll n;
-        cin>>n;
-        cout<<gcd(2, 6);
-        cout<<endl;
-    }    
-    return 0;
+	cin>>t;
+	while(t--){
+		ll n;
+		cin>>n;
+		if(n<=3){
+			cout<<"1"<<endl;
+			cout<<n<<" ";
+			for(int i=1;i<=n;i++)
+				cout<<i<<" ";
+			    cout<<endl;
+		}
+		else if(n%2==1){
+			ll time=((n-3)/2)+1;
+			cout<<time<<endl;
+			cout<<"3"<<" "<<"1"<<" "<<"2"<<" "<<"3"<<endl;
+			for(int i=4;i<n;i+=2)
+				cout<<"2"<<" "<<i<<" "<<(i+1)<<endl;
+		} else {
+			cout<<(n/2)<<endl;
+			for(int i=1;i<n;i+=2)
+				cout<<"2"<<" "<<i<<" "<<(i+1)<<endl;
+		}
+	}
+	return 0;
 }
